0a2638921b1e6070b1a48559b3b26f5b78df9acb
Slice Deployment <deployment@slice.local>
2026-08-12 20:04:00 -0400

    fix: wire admin runtime module dependencies

--- server/src/modules/admin/admin.module.ts
+++ server/src/modules/admin/admin.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../../database/database.module';
 import { AccessControlModule } from '../identity/access/access-control.module';
+import { AuthModule } from '../identity/auth/auth.module';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 
 @Module({
-  imports: [AccessControlModule],
+  imports: [DatabaseModule, AuthModule, AccessControlModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
