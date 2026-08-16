88c078cd13131bfded47fe04da53dac6e35be76d
Slice Deployment <deployment@slice.local>
2026-08-15 21:36:37 -0400

    Wire market storage dependency

--- server/src/modules/market/market.module.ts
+++ server/src/modules/market/market.module.ts
@@ -4,5 +4,12 @@ import { MarketService } from './market.service';
 import { MarketProviderRegistry } from './market-provider.registry';
 import { MarketRefreshService } from './market-refresh.service';
 import { MarketRefreshWorker } from './market-refresh.worker';
-@Module({ controllers: [MarketController], providers: [MarketService, MarketProviderRegistry, MarketRefreshService, MarketRefreshWorker], exports: [MarketService, MarketRefreshService, MarketProviderRegistry] })
+import { SubmissionsModule } from '../submissions/submissions.module';
+
+@Module({
+  imports: [SubmissionsModule],
+  controllers: [MarketController],
+  providers: [MarketService, MarketProviderRegistry, MarketRefreshService, MarketRefreshWorker],
+  exports: [MarketService, MarketRefreshService, MarketProviderRegistry],
+})
 export class MarketModule {}
